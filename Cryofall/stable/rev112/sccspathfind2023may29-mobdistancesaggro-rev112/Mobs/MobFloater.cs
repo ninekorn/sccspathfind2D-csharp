0705f7015e035ec78bee02f4d155d65e592277e4
nine <steve.chasse@gmail.com>
2023-05-29 16:53:28 -0400

    c# pathfind revision - mob aggro distance added.

---
+++ Cryofall/stable/rev112/sccspathfind2023may29-mobdistancesaggro-rev112/Mobs/MobFloater.cs
@@ -0,0 +1,295 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Characters.Mobs
+{
+    using AtomicTorch.CBND.CoreMod.Characters.Player;
+    using AtomicTorch.CBND.CoreMod.CharacterSkeletons;
+    using AtomicTorch.CBND.CoreMod.Events;
+    using AtomicTorch.CBND.CoreMod.Items.Food;
+    using AtomicTorch.CBND.CoreMod.Items.Generic;
+    using AtomicTorch.CBND.CoreMod.Items.Weapons.MobWeapons;
+    using AtomicTorch.CBND.CoreMod.Skills;
+    using AtomicTorch.CBND.CoreMod.SoundPresets;
+    using AtomicTorch.CBND.CoreMod.Stats;
+    using AtomicTorch.CBND.CoreMod.Systems.CharacterDeath;
+    using AtomicTorch.CBND.CoreMod.Systems.Droplists;
+    using AtomicTorch.CBND.GameApi.Data.Characters;
+    using AtomicTorch.CBND.GameApi.Scripting;
+
+
+    using AtomicTorch.CBND.CoreMod.Characters;
+    using AtomicTorch.CBND.CoreMod.CharacterSkeletons;
+    using AtomicTorch.CBND.CoreMod.ConsoleCommands.Admin;
+    using AtomicTorch.CBND.CoreMod.Items.Generic;
+    using AtomicTorch.CBND.CoreMod.Items.Weapons.MobWeapons;
+    using AtomicTorch.CBND.CoreMod.SoundPresets;
+    using AtomicTorch.CBND.CoreMod.Systems.Droplists;
+    using AtomicTorch.CBND.GameApi.Data.Characters;
+    using AtomicTorch.CBND.GameApi.Data.Physics;
+    using AtomicTorch.CBND.GameApi.Scripting;
+    using AtomicTorch.CBND.GameApi.ServicesServer;
+    using AtomicTorch.GameEngine.Common.DataStructures;
+    using AtomicTorch.GameEngine.Common.Primitives;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using static sccspathfindstructs;
+    using sccsvec2int = sccspathfindstructs.sccsvec2int;
+    using AtomicTorch.CBND.CoreMod.Scripts.sccsPathfind;
+
+    public class MobFloater : ProtoCharacterMob
+    {
+        public override bool AiIsRunAwayFromHeavyVehicles => false;
+
+        public override double BiomaterialValueMultiplier => 2.0;
+
+        public override float CharacterWorldHeight => 2.4f;
+
+        public override float CharacterWorldWeaponOffsetRanged => 1.1f;
+
+        public override double MobKillExperienceMultiplier => 5.0;
+
+        public override string Name => "Floater";
+
+        public override ObjectMaterial ObjectMaterial => ObjectMaterial.SoftTissues;
+
+        public override double PhysicsBodyAccelerationCoef => 1;
+
+        public override double PhysicsBodyFriction => 0.5;
+
+        public override double StatDefaultHealthMax => 300;
+
+        public override double StatMoveSpeed => 2.5;
+
+        protected override void FillDefaultEffects(Effects effects)
+        {
+            base.FillDefaultEffects(effects);
+
+            // easy to kill by normal means, but have very tough protection against certain types of damage
+            effects.AddValue(this, StatName.DefenseImpact, 1.0)
+                   .AddValue(this, StatName.DefenseKinetic,   0.2)
+                   .AddValue(this, StatName.DefenseHeat,      0.1)
+                   .AddValue(this, StatName.DefenseChemical,  1.0)
+                   .AddValue(this, StatName.DefensePsi,       1.0)
+                   .AddValue(this, StatName.DefenseRadiation, 1.0);
+        }
+
+        protected override void PrepareProtoCharacterMob(
+            out ProtoCharacterSkeleton skeleton,
+            ref double scale,
+            DropItemsList lootDroplist)
+        {
+            skeleton = GetProtoEntity<SkeletonFloater>();
+
+            // primary loot
+            lootDroplist
+                .Add<ItemInsectMeatRaw>(count: 1, countRandom: 1)
+                .Add<ItemSlime>(count: 10,        countRandom: 10)
+                .Add<ItemToxin>(count: 5,         countRandom: 5)
+                .Add<ItemSalt>(count: 5,          countRandom: 5)
+                .Add<ItemRubberRaw>(count: 8,     countRandom: 4)
+                .Add<ItemKeinite>(count: 2,       countRandom: 2);
+
+            // extra loot
+            lootDroplist.Add(condition: SkillHunting.ServerRollExtraLoot,
+                             nestedList: new DropItemsList(outputs: 2)
+                                         .Add<ItemInsectMeatRaw>(count: 1)
+                                         .Add<ItemSlime>(count: 5)
+                                         .Add<ItemToxin>(count: 2)
+                                         .Add<ItemSalt>(count: 2));
+
+            if (!IsServer)
+            {
+                return;
+            }
+
+            ServerCharacterDeathMechanic.CharacterKilled += ServerCharacterKilledHandler;
+
+            static void ServerCharacterKilledHandler(
+                ICharacter attackerCharacter,
+                ICharacter targetCharacter)
+            {
+                if (!attackerCharacter.IsNpc
+                    && targetCharacter.ProtoCharacter.GetType() == typeof(MobFloater)
+                    && (SharedEventHelper.SharedIsInsideEventArea<EventMigrationFloater>(attackerCharacter.Position)
+                        || SharedEventHelper.SharedIsInsideEventArea<EventMigrationFloater>(targetCharacter.Position)))
+                {
+                    PlayerCharacter.GetPrivateState(attackerCharacter)
+                                   .CompletionistData
+                                   .ServerOnParticipatedInEvent(Api.GetProtoEntity<EventMigrationFloater>());
+                }
+            }
+        }
+
+        protected override void ServerInitializeCharacterMob(ServerInitializeData data)
+        {
+            base.ServerInitializeCharacterMob(data);
+
+            var weaponProto = GetProtoEntity<ItemWeaponMobFloaterNova>();
+            data.PrivateState.WeaponState.SharedSetWeaponProtoOnly(weaponProto);
+            data.PublicState.SharedSetCurrentWeaponProtoOnly(weaponProto);
+        }
+        /*
+        protected override void ServerUpdateMob(ServerUpdateData data)
+        {
+            var character = data.GameObject;
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                character,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character),
+                isRetreating: false,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 0,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 15,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(character, movementDirection, rotationAngleRad);
+        }*/
+
+
+
+        //sccspathfindaction pathfindaction = new sccspathfindaction();
+        protected override void ServerUpdateMob(ServerUpdateData data)
+        {
+            //pathfindaction.engagepathfindaction(data);
+            //CharacterMobPublicState theobjstate = data.PublicState;
+            //ICharacter thegameobject = data.GameObject;
+
+            if (data.PublicState.CurrentStats.indexswtc == -1)
+            {
+                data.PublicState.CurrentStats.indexinmainarray = sccspathfindarrays.ProcessAddToMainList(data.GameObject);
+                data.PublicState.CurrentStats.indexswtc = 1;
+            }
+
+            //sccspathfindactionstruct thedata = sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray];
+
+            sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray] = sccspathfindaction.engagepathfindaction(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+
+            if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 0)
+            {
+                retreatthecharacter(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 1)
+            {
+                chasethecharacter(data.PublicState, data.GameObject);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 2)
+            {
+                movetopathfindnode(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 3)
+            {
+                stopstabilizethecharacter(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            //pathfindpermobdatalight
+
+        }
+        private void stopstabilizethecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+            sccsvec2float dirright = new sccsvec2float();
+            dirright.x = 1.0f;
+            dirright.y = 0.0f;
+
+            /*sccsvec2float dirnpctoplayer = new sccsvec2float();
+            dirnpctoplayer.x = currentnpcglobaldata.playerpos.x - currentnpcglobaldata.npcpos.x;
+            dirnpctoplayer.y = currentnpcglobaldata.playerpos.y - currentnpcglobaldata.npcpos.y;
+
+            dirnpctoplayer.x /= currentnpcglobaldata.distnpctoplayersqrt;
+            dirnpctoplayer.y /= currentnpcglobaldata.distnpctoplayersqrt;*/
+
+            float anglerad = Vector2F.AngleDeg(new Vector2F(sccspathfindactionstruct.direction.X, sccspathfindactionstruct.direction.Y), new Vector2F(dirright.x, dirright.y));
+
+            ServerCharacterAiHelper.ProcessRetreatingAi(thegameobject, 0.25f, out var movementDirection0, out var rotationAngleRad0); //distnpctoplayersqrt
+
+            this.ServerSetMobInput(thegameobject, movementDirection0, sccspathfindactionstruct.anglerad);
+
+
+            //sccspathfindactionstruct.movementoptiontype = 3;
+            //sccspathfindactionstruct.anglerad = anglerad;
+            //sccspathfindactionstruct.direction = currentnpcglobaldata.directionnpctopathfindnodef;
+
+            var character1 = thegameobject;
+            var currentStats1 = theobjstate.CurrentStats;
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                character1,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character1),
+                isRetreating: currentStats1.HealthCurrent < currentStats1.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(character1, movementDirection, rotationAngleRad);
+        }
+
+        private void movetopathfindnode(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                thegameobject,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(thegameobject),
+                isRetreating: theobjstate.CurrentStats.HealthCurrent < theobjstate.CurrentStats.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(thegameobject, sccspathfindactionstruct.direction, rotationAngleRad);//sccspathfindactionstruct.anglerad);
+        }
+
+
+
+        private void chasethecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject)//ServerUpdateData data)
+        {
+            var character1 = thegameobject;
+            var currentStats1 = theobjstate.CurrentStats;
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                character1,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character1),
+                isRetreating: currentStats1.HealthCurrent < currentStats1.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(character1, movementDirection, rotationAngleRad);
+        }
+
+
+        private void retreatthecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+            sccsvec2float dirright = new sccsvec2float();
+            dirright.x = 1.0f;
+            dirright.y = 0.0f;
+
+            /*sccsvec2float dirnpctoplayer = new sccsvec2float();
+            dirnpctoplayer.x = currentnpcglobaldata.playerpos.x - currentnpcglobaldata.npcpos.x;
+            dirnpctoplayer.y = currentnpcglobaldata.playerpos.y - currentnpcglobaldata.npcpos.y;
+
+            dirnpctoplayer.x /= currentnpcglobaldata.distnpctoplayersqrt;
+            dirnpctoplayer.y /= currentnpcglobaldata.distnpctoplayersqrt;*/
+
+            float anglerad = Vector2F.AngleDeg(new Vector2F(sccspathfindactionstruct.direction.X, sccspathfindactionstruct.direction.Y), new Vector2F(dirright.x, dirright.y));
+
+            ServerCharacterAiHelper.ProcessRetreatingAi(thegameobject, 0.25f, out var movementDirection0, out var rotationAngleRad); //distnpctoplayersqrt
+
+            this.ServerSetMobInput(thegameobject, movementDirection0, rotationAngleRad);// sccspathfindactionstruct.anglerad);
+            //sccspathfindactionstruct.movementoptiontype = 3;
+            //sccspathfindactionstruct.anglerad = anglerad;
+            //sccspathfindactionstruct.direction = currentnpcglobaldata.directionnpctopathfindnodef;
+
+        }
+
+
+
+    }
+}
---
+++ Cryofall/stable/rev112/sccspathfind2023may29-mobdistancesaggro-rev112/Mobs/MobWildBoar.cs
@@ -0,0 +1,270 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Characters.Mobs
+{
+    using AtomicTorch.CBND.CoreMod.CharacterSkeletons;
+    using AtomicTorch.CBND.CoreMod.Items.Food;
+    using AtomicTorch.CBND.CoreMod.Items.Generic;
+    using AtomicTorch.CBND.CoreMod.Items.Weapons.MobWeapons;
+    using AtomicTorch.CBND.CoreMod.Skills;
+    using AtomicTorch.CBND.CoreMod.SoundPresets;
+    using AtomicTorch.CBND.CoreMod.Stats;
+    using AtomicTorch.CBND.CoreMod.Systems.Droplists;
+
+    using AtomicTorch.CBND.CoreMod.Characters;
+    using AtomicTorch.CBND.CoreMod.ConsoleCommands.Admin;
+
+    using sccspathfindnode = sccspathfindstructs.sccspathfindnode;
+    using sccspathfindcombineddata = sccspathfindstructs.sccspathfindcombineddata;
+    using sccspathfindgriddatafinal = sccspathfindstructs.sccspathfindgriddatafinal;
+    using sccspathfindgridworldindex = sccspathfindstructs.sccspathfindgridworldindex;
+    using sccspathfindgridWorldSize = sccspathfindstructs.sccspathfindgridWorldSize;
+    using sccspathfindcheckallsidesdata = sccspathfindstructs.sccspathfindcheckallsidesdata;
+    using sccspathfindneighboortile = sccspathfindstructs.sccspathfindneighboortile;
+    using sccspathfindextraTiles = sccspathfindstructs.sccspathfindextraTiles;
+    using sccspathfindobstacledata = sccspathfindstructs.sccspathfindobstacledata;
+    using sccsvec2int = sccspathfindstructs.sccsvec2int;
+    using static sccspathfindstructs;
+    using static sccsvestationoutpost;
+
+    using AtomicTorch.CBND.GameApi.Data.Physics;
+    using System.Windows.Documents;
+    using AtomicTorch.CBND.GameApi.Scripting;
+    using AtomicTorch.CBND.GameApi.ServicesServer;
+
+
+    using System.Collections.Generic;
+    using System.Collections;
+    using System;
+    using AtomicTorch.GameEngine.Common.Primitives;
+
+    using AtomicTorch.CBND.GameApi.Data.Characters;
+    using AtomicTorch.GameEngine.Common.DataStructures;
+    using AtomicTorch.CBND.CoreMod.Scripts.sccsPathfind;
+
+    public class MobWildBoar : ProtoCharacterMob
+    {
+        private static readonly IWorldServerService ServerWorldService
+            = Api.IsServer
+                  ? Api.Server.World
+                  : null;
+        public override bool AiIsRunAwayFromHeavyVehicles => true;
+
+        public override float CharacterWorldHeight => 1f;
+
+        public override double MobKillExperienceMultiplier => 1.0;
+
+        public override string Name => "Wild boar";
+
+        public override ObjectMaterial ObjectMaterial => ObjectMaterial.SoftTissues;
+
+        public override double StatDefaultHealthMax => 80;
+
+        public override double StatMoveSpeed => 1.9;
+
+        protected override void FillDefaultEffects(Effects effects)
+        {
+            base.FillDefaultEffects(effects);
+
+            effects.AddValue(this, StatName.DefenseImpact, 0.4);
+        }
+
+        protected override void PrepareProtoCharacterMob(
+            out ProtoCharacterSkeleton skeleton,
+            ref double scale,
+            DropItemsList lootDroplist)
+        {
+            skeleton = GetProtoEntity<SkeletonWildBoar>();
+
+            // primary loot
+            lootDroplist
+                .Add<ItemMeatRaw>(count: 1)
+                .Add<ItemLeather>(count: 1, countRandom: 1);
+
+            // random loot
+            lootDroplist.Add(nestedList: new DropItemsList(outputs: 1)
+                                         .Add<ItemAnimalFat>(count: 1, countRandom: 1)
+                                         .Add<ItemBones>(count: 1,     countRandom: 1));
+
+            // extra loot
+            lootDroplist.Add(condition: SkillHunting.ServerRollExtraLoot,
+                             nestedList: new DropItemsList(outputs: 2)
+                                         .Add<ItemMeatRaw>(count: 1)
+                                         .Add<ItemLeather>(count: 1)
+                                         .Add<ItemAnimalFat>(count: 1)
+                                         .Add<ItemBones>(count: 1));
+        }
+
+        protected override void ServerInitializeCharacterMob(ServerInitializeData data)
+        {
+            base.ServerInitializeCharacterMob(data);
+
+            var weaponProto = GetProtoEntity<ItemWeaponMobGenericMedium>();
+            data.PrivateState.WeaponState.SharedSetWeaponProtoOnly(weaponProto);
+            data.PublicState.SharedSetCurrentWeaponProtoOnly(weaponProto);
+        }
+
+
+
+        /*
+            protected override void ServerUpdateMob(ServerUpdateData data)
+            {
+                var character = data.GameObject;
+
+                ServerCharacterAiHelper.ProcessAggressiveAi(
+                    character,
+                    targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character),
+                    isRetreating: false,
+                    isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                    distanceRetreat: 0,
+                    distanceEnemyTooClose: 1,
+                    distanceEnemyTooFar: 5,
+                    movementDirection: out var movementDirection,
+                    rotationAngleRad: out var rotationAngleRad);
+
+                this.ServerSetMobInput(character, movementDirection, rotationAngleRad);
+            }*/
+
+
+        //sccspathfindaction pathfindaction = new sccspathfindaction();
+        protected override void ServerUpdateMob(ServerUpdateData data)
+        {
+            //pathfindaction.engagepathfindaction(data);
+            //CharacterMobPublicState theobjstate = data.PublicState;
+            //ICharacter thegameobject = data.GameObject;
+
+            if (data.PublicState.CurrentStats.indexswtc == -1)
+            {
+                data.PublicState.CurrentStats.indexinmainarray = sccspathfindarrays.ProcessAddToMainList(data.GameObject);
+                data.PublicState.CurrentStats.indexswtc = 1;
+            }
+
+            //sccspathfindactionstruct thedata = sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray];
+
+            sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray] = sccspathfindaction.engagepathfindaction(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+
+            if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 0)
+            {
+                retreatthecharacter(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 1)
+            {
+                chasethecharacter(data.PublicState, data.GameObject);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 2)
+            {
+                movetopathfindnode(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            else if (sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray].movementoptiontype == 3)
+            {
+                stopstabilizethecharacter(data.PublicState, data.GameObject, sccspathfindarrays.pathfindpermobdatalight[data.PublicState.CurrentStats.indexinmainarray]);
+            }
+            //pathfindpermobdatalight
+
+        }
+        private void stopstabilizethecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+            sccsvec2float dirright = new sccsvec2float();
+            dirright.x = 1.0f;
+            dirright.y = 0.0f;
+
+            /*sccsvec2float dirnpctoplayer = new sccsvec2float();
+            dirnpctoplayer.x = currentnpcglobaldata.playerpos.x - currentnpcglobaldata.npcpos.x;
+            dirnpctoplayer.y = currentnpcglobaldata.playerpos.y - currentnpcglobaldata.npcpos.y;
+
+            dirnpctoplayer.x /= currentnpcglobaldata.distnpctoplayersqrt;
+            dirnpctoplayer.y /= currentnpcglobaldata.distnpctoplayersqrt;*/
+
+            float anglerad = Vector2F.AngleDeg(new Vector2F(sccspathfindactionstruct.direction.X, sccspathfindactionstruct.direction.Y), new Vector2F(dirright.x, dirright.y));
+
+            ServerCharacterAiHelper.ProcessRetreatingAi(thegameobject, 0.25f, out var movementDirection0, out var rotationAngleRad0); //distnpctoplayersqrt
+
+            this.ServerSetMobInput(thegameobject, movementDirection0, sccspathfindactionstruct.anglerad);
+
+
+            //sccspathfindactionstruct.movementoptiontype = 3;
+            //sccspathfindactionstruct.anglerad = anglerad;
+            //sccspathfindactionstruct.direction = currentnpcglobaldata.directionnpctopathfindnodef;
+
+            var character1 = thegameobject;
+            var currentStats1 = theobjstate.CurrentStats;
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                character1,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character1),
+                isRetreating: currentStats1.HealthCurrent < currentStats1.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(character1, movementDirection, rotationAngleRad);
+        }
+
+        private void movetopathfindnode(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                thegameobject,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(thegameobject),
+                isRetreating: theobjstate.CurrentStats.HealthCurrent < theobjstate.CurrentStats.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(thegameobject, sccspathfindactionstruct.direction, rotationAngleRad);//sccspathfindactionstruct.anglerad);
+        }
+
+
+
+        private void chasethecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject)//ServerUpdateData data)
+        {
+            var character1 = thegameobject;
+            var currentStats1 = theobjstate.CurrentStats;
+
+            ServerCharacterAiHelper.ProcessAggressiveAi(
+                character1,
+                targetCharacter: ServerCharacterAiHelper.GetClosestTargetPlayer(character1),
+                isRetreating: currentStats1.HealthCurrent < currentStats1.HealthMax / 3,
+                isRetreatingForHeavyVehicles: this.AiIsRunAwayFromHeavyVehicles,
+                distanceRetreat: 7,
+                distanceEnemyTooClose: 1,
+                distanceEnemyTooFar: 3.5,
+                movementDirection: out var movementDirection,
+                rotationAngleRad: out var rotationAngleRad);
+
+            this.ServerSetMobInput(character1, movementDirection, rotationAngleRad);
+        }
+
+
+        private void retreatthecharacter(CharacterMobPublicState theobjstate, ICharacter thegameobject, sccspathfindactionstruct sccspathfindactionstruct)//ServerUpdateData data)
+        {
+            sccsvec2float dirright = new sccsvec2float();
+            dirright.x = 1.0f;
+            dirright.y = 0.0f;
+
+            /*sccsvec2float dirnpctoplayer = new sccsvec2float();
+            dirnpctoplayer.x = currentnpcglobaldata.playerpos.x - currentnpcglobaldata.npcpos.x;
+            dirnpctoplayer.y = currentnpcglobaldata.playerpos.y - currentnpcglobaldata.npcpos.y;
+
+            dirnpctoplayer.x /= currentnpcglobaldata.distnpctoplayersqrt;
+            dirnpctoplayer.y /= currentnpcglobaldata.distnpctoplayersqrt;*/
+
+            float anglerad = Vector2F.AngleDeg(new Vector2F(sccspathfindactionstruct.direction.X, sccspathfindactionstruct.direction.Y), new Vector2F(dirright.x, dirright.y));
+
+            ServerCharacterAiHelper.ProcessRetreatingAi(thegameobject, 0.25f, out var movementDirection0, out var rotationAngleRad); //distnpctoplayersqrt
+
+            this.ServerSetMobInput(thegameobject, movementDirection0, rotationAngleRad);// sccspathfindactionstruct.anglerad);
+
+            //sccspathfindactionstruct.movementoptiontype = 3;
+            //sccspathfindactionstruct.anglerad = anglerad;
+            //sccspathfindactionstruct.direction = currentnpcglobaldata.directionnpctopathfindnodef;
+
+        }
+
+
+    }
+}
