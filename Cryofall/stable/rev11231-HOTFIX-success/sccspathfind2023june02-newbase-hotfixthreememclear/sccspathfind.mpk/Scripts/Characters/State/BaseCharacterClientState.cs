e3a669b74def69182662831aca102cc87298908c
nine <steve.chasse@gmail.com>
2023-06-02 03:04:29 -0400

    rev1.1231 - hotfixthree
    i apologize, it was working only for the wolf/tropicalboar/wildboar/greensnake. I added the hotfixthree to rest of the mobs

---
+++ Cryofall/stable/rev11231-HOTFIX-success/sccspathfind2023june02-newbase-hotfixthreememclear/sccspathfind.mpk/Scripts/Characters/State/BaseCharacterClientState.cs
@@ -0,0 +1,46 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Characters
+{
+    using System.Collections.Generic;
+    using AtomicTorch.CBND.CoreMod.CharacterSkeletons;
+    using AtomicTorch.CBND.CoreMod.Helpers.Client;
+    using AtomicTorch.CBND.CoreMod.Helpers.Primitives;
+    using AtomicTorch.CBND.GameApi.Data.Items;
+    using AtomicTorch.CBND.GameApi.Data.State;
+    using AtomicTorch.CBND.GameApi.Scripting.ClientComponents;
+    using AtomicTorch.CBND.GameApi.ServicesClient.Components;
+
+    public class BaseCharacterClientState : BaseClientState, IClientStateWithShadowRenderer
+    {
+        [SubscribableProperty]
+        public ProtoCharacterSkeleton CurrentProtoSkeleton { get; set; }
+
+        public double CurrentProtoSkeletonScaleMultiplier { get; set; }
+
+        public bool HasWeaponAnimationAssigned { get; set; }
+
+        public bool? IsDead { get; set; }
+
+        public bool IsWeaponFiringAnimationActive { get; set; }
+
+        public float LastAimCoef { get; set; }
+
+        [SubscribableProperty]
+        public ushort? LastEquipmentContainerHash { get; set; }
+
+        public double? LastInterpolatedRotationAngleRad { get; set; }
+
+        public IItem LastSelectedItem { get; set; }
+
+        public ViewOrientation LastViewOrientation { get; set; }
+
+        public IComponentSpriteRenderer RendererShadow { get; set; }
+
+        public List<IClientComponent> SkeletonComponents { get; } = new();
+
+        public IComponentSkeleton SkeletonRenderer { get; set; }
+
+        public IComponentSoundEmitter SoundEmitterLoopCharacter { get; set; }
+
+        public IComponentSoundEmitter SoundEmitterLoopMovemement { get; set; }
+    }
+}
---
+++ Cryofall/stable/rev11231-HOTFIX-success/sccspathfind2023june02-newbase-hotfixthreememclear/sccspathfind.mpk/Scripts/Characters/State/CharacterMobPrivateState.cs
@@ -0,0 +1,37 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Characters
+{
+    using AtomicTorch.CBND.GameApi.Data.Characters;
+    using AtomicTorch.CBND.GameApi.Data.State;
+    using AtomicTorch.GameEngine.Common.Primitives;
+
+    public class CharacterMobPrivateState : BaseCharacterPrivateState
+    {
+        [TempOnly]
+        public double AttackRange { get; set; }
+
+        [TempOnly]
+        public ICharacter CurrentAggroCharacter { get; set; }
+
+        [TempOnly]
+        public double CurrentAggroTimeRemains { get; set; }
+
+        [TempOnly]
+        public ICharacter CurrentTargetCharacter { get; set; }
+
+        [TempOnly]
+        public bool IsRetreating { get; set; }
+
+        [TempOnly]
+        public double LastFleeSoundTime { get; set; }
+
+        [TempOnly]
+        public Vector2Ushort SpawnPosition { get; set; }
+
+        /// <summary>
+        /// This timer is incremented when the mob is too far from the spawn location.
+        /// As it reaches a certain high number the mob should be despawned.
+        /// </summary>
+        [TempOnly]
+        public double TimerDespawn { get; set; }
+    }
+}
