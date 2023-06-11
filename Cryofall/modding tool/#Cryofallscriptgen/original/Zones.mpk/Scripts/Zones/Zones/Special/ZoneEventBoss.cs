6e1c8b813a4f13cbfedf17c11a494dc48085c1f7
nine <steve.chasse@gmail.com>
2023-06-11 00:46:52 -0400

    update - i actually had forgotten to upload the assets. here they are now.

---
+++ Cryofall/modding tool/#Cryofallscriptgen/original/Zones.mpk/Scripts/Zones/Zones/Special/ZoneEventBoss.cs
@@ -0,0 +1,17 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Zones
+{
+    using AtomicTorch.CBND.GameApi;
+
+    public class ZoneEventBoss : ProtoZoneDefault
+    {
+        [NotLocalizable]
+        public override string Name => "Special - Event - Boss spawn";
+
+        protected override void PrepareZone(ZoneScripts scripts)
+        {
+            // Special zone - no logic here
+
+            //9SMODS
+        }
+    }
+}
---
+++ Cryofall/modding tool/#Cryofallscriptgen/original/Zones.mpk/Scripts/Zones/Zones/Temperate/ZoneTemperateClay.cs
@@ -0,0 +1,24 @@
+﻿namespace AtomicTorch.CBND.CoreMod.Zones
+{
+    using AtomicTorch.CBND.GameApi;
+
+    public class ZoneTemperateClay : ProtoZoneDefault
+    {
+        [NotLocalizable]
+        public override string Name => "Temperate - Clay pit";
+
+        protected override void PrepareZone(ZoneScripts scripts)
+        {
+            // minerals
+            scripts
+                .Add(GetScript<SpawnResourceClay>());
+
+            // mobs
+            scripts
+                .Add(GetScript<SpawnMobsCloakedLizard>())
+                .Add(GetScript<SpawnMobsCrawler>());
+
+            //9SMODS
+        }
+    }
+}
